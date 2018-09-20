34aa95128c7153b2041e181ad7411ff68a9bd937
Michael Allan <Michael Allan>
2018-09-20 17:13:42 +1000

    Implement info addon in Storybook, and enhance Button story

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
 
 import { Button } from './Button'
 
 storiesOf('Button', module)
-  .addWithJSX('with background', () => (
+  .addWithJSX('with background', withInfo(
+    'description of Button component'
+  )(() => (
     <Button bg="orange">Hello world</Button>
-  ))
+  )))
   .addWithJSX('with a different background', () => (
     <Button bg="palegoldenrod">Hello world</Button>
   ))

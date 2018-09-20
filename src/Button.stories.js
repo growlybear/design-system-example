f4d6dc45cf47a9423b6118ab729d541558dd0c07
Michael Allan <Michael Allan>
2018-09-20 12:54:36 +1000

    Additional story for Button bg

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -3,6 +3,10 @@ import { storiesOf } from '@storybook/react'
 
 import { Button } from './Button'
 
-storiesOf('Button', module).add('with background', () => (
-  <Button bg="orange">Hello world</Button>
-))
+storiesOf('Button', module)
+  .add('with background', () => (
+    <Button bg="orange">Hello world</Button>
+  ))
+  .add('with a different background', () => (
+    <Button bg="palegoldenrod">Hello world</Button>
+  ))
